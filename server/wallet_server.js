1d3cdca5931e459fee4fc3c37b62ba2e47632009
truesty <ngaborodrigue@gmail.com>
2025-01-19 10:58:16 -0500

    Fix deploy

--- server/wallet_server.js
+++ server/wallet_server.js
@@ -126,7 +126,7 @@ app.post("/api/budgets", (req, res) => {
 });
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

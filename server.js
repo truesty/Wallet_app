60011c1f9186246d3814ba4b591011ef21a354c1
truesty <ngaborodrigue@gmail.com>
2025-01-21 10:44:48 -0500

    Updated server configuration for Railway deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
@@ -201,5 +201,5 @@ app.get('*', (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 }); 
